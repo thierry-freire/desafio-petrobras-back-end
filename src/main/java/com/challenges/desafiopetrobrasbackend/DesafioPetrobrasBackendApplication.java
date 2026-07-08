4fe8863a7fa428d8509e3c8575b43412fe02e64c
thierry-freire <thierry.freire@hotmail.com>
2026-07-08 14:50:09 -0300

    Initial commit

---
+++ src/main/java/com/challenges/desafiopetrobrasbackend/DesafioPetrobrasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.challenges.desafiopetrobrasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPetrobrasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioPetrobrasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenges/desafiopetrobrasbackend/TestcontainersConfiguration.java
@@ -0,0 +1,25 @@
+package com.challenges.desafiopetrobrasbackend;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+    @Bean
+    @ServiceConnection
+    MySQLContainer mysqlContainer() {
+        return new MySQLContainer(DockerImageName.parse("mysql:latest"));
+    }
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer postgresContainer() {
+        return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+    }
+
+}
