1dbac7870d5057ff612b3d9b4f1e9464c01dcea5
thierry-freire <thierry.freire@hotmail.com>
2026-07-09 15:15:40 -0300

    Ajustes no código de retorno do POST.

--- src/main/java/com/challenges/desafiopetrobrasbackend/controller/EventosController.java
+++ src/main/java/com/challenges/desafiopetrobrasbackend/controller/EventosController.java
@@ -5,6 +5,7 @@ import com.challenges.desafiopetrobrasbackend.model.Eventos;
 import com.challenges.desafiopetrobrasbackend.services.EventosService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +27,6 @@ public class EventosController {
     @PostMapping("/events")
     public ResponseEntity<Eventos> save (@RequestBody Eventos evento) {
         eventosService.save(evento);
-        return new ResponseEntity<>(HttpStatusCode.valueOf(201));
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
